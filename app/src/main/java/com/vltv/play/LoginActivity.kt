625e917c64124462e6002c3bf6fdf5b712fde9b4
leogracas34-gif <leogracas34@gmail.com>
2026-02-24 20:39:55 -0300

    Update LoginActivity.kt

--- app/src/main/java/com/vltv/play/LoginActivity.kt
+++ app/src/main/java/com/vltv/play/LoginActivity.kt
@@ -152,7 +152,7 @@ class LoginActivity : AppCompatActivity() {
                     salvarCredenciais(dnsVencedor!!, user, pass)
                     
                     withContext(Dispatchers.Main) {
-                    // Toast.makeText(this@LoginActivity, "Conectado em $dnsVencedor", Toast.LENGTH_LONG).show()
+                        Toast.makeText(this@LoginActivity, "Conectado em $dnsVencedor", Toast.LENGTH_LONG).show()
                     }
                     
                     preCarregarConteudoInicial(dnsVencedor!!, user, pass)
